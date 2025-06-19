a374baf7fd0ad6d9839d2d5aff0eb1faaf179328
xuziran <xuziran687@gmail.com>
2025-06-19 09:56:34 +0800

    first commit

---
+++ src/test/java/com/xuziran/GitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuziran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
